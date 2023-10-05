99b6d2cfd551d8d1a04c7eb4c523119b52bf1ffe
= <=>
2023-10-05 21:07:42 +0500

    Practicing Js

---
+++ index.js
@@ -0,0 +1,90 @@
+//let name = 'zaibi';
+//console.log(name)
+
+//let firstName = 'Uzair';
+//let SecondtName = 'Shaikh';
+//console.log(firstName);
+//console.log(SecondtName);
+
+//let rate1 = 1;
+//rate = 2 ;
+//console.log(rate1);
+
+//let Name = 'zaibi' //String
+//let age = 25; //number
+//let approved = true; //boolean
+//console.log(Name +' '+ age +' '+ approved);
+
+//let Name01 = 'Uzair';
+//let age1 = 20;
+//let approved1 = true;
+//let firstName1 = undefined;
+//let colors =null; //object
+
+//let Person = {
+//name :'zaibi'
+//age : 25};
+
+//console.log(Person);
+
+//Person.name = 'shaikh'
+
+//console.log(Person.name);
+
+//let selectedcolors = ['orange', 'Blue'];
+//selectedcolors[2] = 'red';
+//console.log(selectedcolors.length) ;
+
+//fUNCTION 1
+//function greet(Name, lastname) {
+//console.log("Hello" + " " + Name + " " + lastname);
+//}
+//greet("uzair", "shaikh");
+
+//function preet(Name01, Name2) {
+//console.log(Name01 + " " + Name2);
+//}
+//preet("Hows", "You");
+
+//Function2TYPECALCALCULATED
+//function gulpreet(number) {
+//return number * number;
+//}
+//console.log(gulpreet(2));
+
+//-------Arithmetic operators--------------
+
+//let x = 10;
+//let y = 5;
+
+//console.log(x + y);
+//console.log(x - y);
+//console.log(x / y);
+//console.log(x % y);
+//console.log(x ** y);
+
+//console.log(--x);
+//console.log(++x);
+//console.log(++x);
+//console.log(++x);
+
+//-----assigmnment operators------
+
+//let x = 10;
+//console.log((x = x + 5));
+//console.log(x + 5);
+//console.log((x *= 3));
+
+//-------------Relational Operators-----------------
+
+//let x = 2;
+//console.log(x > 0);
+//console.log(x > 4);
+//console.log(x <= 2);
+//console.log(x >= 2);
+
+//-----------------Equality-------------------------
+
+let x = 1;
+console.log(x === 1);
+console.log(x !== 1);
