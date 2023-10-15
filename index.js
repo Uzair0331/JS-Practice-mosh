c775b5c20d56799324e865e417364304db8a16a3
Uzair0331 <uxair0331xhaikh@gmail.com>
2023-10-15 13:15:20 +0500

    for loop

--- index.js
+++ index.js
@@ -22,7 +22,6 @@
 //let colors =null; //object
 //---------------------------------
 
-
 //let Person = {
 //name :'zaibi'
 //age : 25};
@@ -43,7 +42,6 @@
 //}
 //greet("uzair", "shaikh");
 
-
 //function preet(Name01, Name2) {
 //console.log(Name01 + " " + Name2);
 //}
@@ -55,11 +53,10 @@
 //}
 //console.log(gulpreet(2));
 
-
 //--------------
 
 //function next (Name, lastname){
-  //console.log('Hello'+" "+Name+" "+lastname)
+//console.log('Hello'+" "+Name+" "+lastname)
 //}
 //next("uzair","shaikh")
 //-------Arithmetic operators--------------
@@ -181,15 +178,15 @@
 
 // value > 1 ? (value === 5 ? console.log("equal") : console.log("bigger")) : console.log("smaller");
 
- //const value = 2;
+//const value = 2;
 
- //if (value === 1) {
-   //console.log("value is equal to 1");
- //} else if (value === 2) {
-  // console.log("value is equal to 2");
- //} else {
- //  console.log("the value is greater than 2");
- //}
+//if (value === 1) {
+//console.log("value is equal to 1");
+//} else if (value === 2) {
+// console.log("value is equal to 2");
+//} else {
+//  console.log("the value is greater than 2");
+//}
 
 //const a = 4;
 //const b = 5;
@@ -200,7 +197,7 @@
 //if (a < b) {
 //  console.log("A smaller");
 //} else if (a === b) {
- // console.log("a equal to b");
+// console.log("a equal to b");
 //} else {
 //  console.log("a greater");
 //}
@@ -213,7 +210,7 @@
 //else if (a === b)
 //console.log('same value')
 //else{
- // console.log('A bigger');
+// console.log('A bigger');
 //}
 //---------Swapping variales--------
 //let x = 'Joe';
@@ -226,7 +223,6 @@
 //console.log(x);
 //console.log(y);
 
-
 //let a = 'pink';
 //let b = 'blue';
 
@@ -235,8 +231,6 @@
 //b=c;
 //console.log(a);
 //console.log(b);
-
-
 
 //---------if....else------time
 
@@ -250,11 +244,7 @@
 //else
 //{console.log('Good evening')}
 
-
-
-
 //---------Practicse--------
-
 
 //let grade =50;
 //if   (grade >= 90 && grade < 100)
@@ -271,45 +261,37 @@
 
 //else if(grade >=50 && grade < 60)
 //console.log('grade C')
-    
+
 //else if(grade >=33 && grade < 50)
 //console.log('grade D')
-     
+
 //else
 //console.log('Fail')
-
-
 
 //--------------------Loop--------------------
 //------------------for loop----------------
 
 //for (let i = 1; i <=5; i++) {
-  //console.log('hello',i);
-  //}
-
-
+//console.log('hello',i);
+//}
 
 //---------------odd Numbers-----------------
 
 //for (let i = 1; i <=10; i++) {
-  //if(i % 2 !==0)
-  //console.log(i);
-    //}  
+//if(i % 2 !==0)
+//console.log(i);
+//}
 
 //------------------Practices------------------
 //for (let i = 1; i <=10 ; i++) {
-  //console.log('hello',i);
+//console.log('hello',i);
 //}
-
-
 
 //---------------even numbers------------------
 //for (let i = 1; i <=10 ; i++) {
-  //if(i % 2 ===0)
-  //console.log(i);
-  //}
-
-
+//if(i % 2 ===0)
+//console.log(i);
+//}
 
 //------------odd Number---------------------
 //for (let i = 1; i <=20 ; i++) {
@@ -317,14 +299,10 @@
 //console.log(i);
 //}
 
-
 //---------------------------------------------
 //for (let i = 1; i <=10 ; i++) {
 //console.log('hey',i);
 //}
-
-
-
 
 //-------11-10-23
 
@@ -333,22 +311,20 @@
 //console.log('Good morning')
 //}
 //else if(12 >=time && time<=18){
- // console.log('Good afternoon')
+// console.log('Good afternoon')
 //}
 //else{
- // console.log('good night')
+// console.log('good night')
 //}
-
-
 
 //let grade = 90;
 
 //if (grade >=90 && grade<100) {
-//console.log('Grade A+1');  
+//console.log('Grade A+1');
 //}
 //else if (grade >=80 && grade <90) {
- //console.log('Grade A1'); 
-//} 
+//console.log('Grade A1');
+//}
 //else if (grade >=70 && grade <80){
 // console.log('Grade A');
 //}
@@ -356,21 +332,20 @@
 //console.log('Grade B');
 //}
 //else if(grade >=50 && grade <60){
- // console.log('Grade c');
+// console.log('Grade c');
 //}
 //else if(grade >=35 && grade <50){
 //console.log('Grade D');
 //}
 //else {
-  //console.log('fail');
+//console.log('fail');
 //}
 
 //---------------------------------------
 
-
 //let time =6;
 //if(time >=6 && time<12){
-  //console.log('Good Morning')
+//console.log('Good Morning')
 //}
 //else if(time >=12 && time<18){
 //console.log('Good Aftenoon')
@@ -379,24 +354,20 @@
 //console.log('Good Night')
 //}
 
-
-
-//-------------------- 
+//--------------------
 
 //let time2 =17 ;
 //if(time2 >=6 && time2 <12){
-//console.log('Good Morning'); 
+//console.log('Good Morning');
 //}
 //else if (time2 >=12 && time2 <18){
- // console.log('Good Afternoon');
+// console.log('Good Afternoon');
 //}
 //else{
- // console.log('Good Night')
+// console.log('Good Night')
 //}
 
-
 //---------------------
-
 
 //let grade =92;
 //if (grade >=90 && grade <100){
@@ -412,13 +383,13 @@
 //console.log('Grade B');
 //}
 //else if (grade >=50 && grade <60){
-//console.log('Grade c');  
+//console.log('Grade c');
 //}
 //else if (grade >=35 && grade <50){
 //console.log('Grade d')
 //}
 //else{
-//console.log('Grade f')  
+//console.log('Grade f')
 //}
 
 //let n =99;
@@ -428,7 +399,6 @@
 //else{
 //  console.log('less then 100');
 //}
-
 
 //let a = 2;
 //let b = 4;
@@ -444,111 +414,94 @@
 //-----------for loop prac-------------
 
 //for (let i = 1; i <= 5; i++) {
-  //console.log('hello world',i);
-  
+//console.log('hello world',i);
+
 //}
 
 //-------------------------------
 //for (let i = 1; i <= 10; i++) {
-  //if(i % 2 !==0)
-  //console.log(i);}
+//if(i % 2 !==0)
+//console.log(i);}
 
 //----------------------------
 //for (let i = 1; i <=20; i++) {
-  //if(i % 2 ==0)
-  //console.log(i); 
+//if(i % 2 ==0)
+//console.log(i);
 //}
 //--------------------------
 
-
 //for (let i = 1; i <=10; i++) {
- //if(i % 2 ==0)
-  //console.log(i);
-  
+//if(i % 2 ==0)
+//console.log(i);
+
 //}
 
 //for (let i = 0; i <=10; i++) {
-  //if(i % 2 !==0)
-  //console.log(i);
-  
+//if(i % 2 !==0)
+//console.log(i);
+
 //}
 
 //for (let i = 1; i <=5; i++) {
 //  console.log('hello ',i);
-  
-//}
 
+//}
 
 //for (let i =1 ; i <=10; i++){
-  //if(i%2 ==0)
-  //console.log(i);
- 
+//if(i%2 ==0)
+//console.log(i);
+
 //}
 
-
 //for (let i = 1; i <= 10; i++) {
-  //if(i %2 !==0)
-  //console.log(i);
-  //}
-
-
-
-
+//if(i %2 !==0)
+//console.log(i);
+//}
 
 //----------while Loop---------------
 //let i =1;
 //while (i <= 5){
-  //if (i % 2 !==0) console.log(i);
-  //i++;
+//if (i % 2 !==0) console.log(i);
+//i++;
 //}
 //-------------practice--------------------
 //let i = 1
 //while (i<=10) {
 //if(i%2 ==0) console.log(i);
 //i++;
- // }
+// }
 //--------------------------------
 //let a = 1
 //while (a<=5) {
 //  console.log('hey',a);
- // a++;
-//}  
+// a++;
+//}
 //----------------------
 //let c = 1
 //while (c<=5) {
- // if (c %2 !==0) console.log(c);
- // c++; 
+// if (c %2 !==0) console.log(c);
+// c++;
 //}
 //-----------------------
 //let d = 1
 //while (d <=10) {
 // if(d%2 !==0)console.log(d);
- //d++;
-  //}
+//d++;
+//}
 
-
-
-
-
-//--------Do while loop-----------  
+//--------Do while loop-----------
 //let i = 0 ;
 //do{
- // if(i%2 !==0)console.log(i);
- //i++;
+// if(i%2 !==0)console.log(i);
+//i++;
 //}while(i <= 5);
 
 //--------------------------
 //let a = 0;
 //do{
- // if(a%2 ==0)console.log(a);
- //a++;
+// if(a%2 ==0)console.log(a);
+//a++;
 //}while(a <=10);
-
-
-
-
-
-
 
 ///---------12-10-23-----practice
 //------Function------
@@ -563,21 +516,15 @@
 //}
 //nav('great','century')
 
-
-
 //-------Tenary operater----
-
-
 
 //let points =91;
 //let Type = points >90 ? "gold" : "silver";
 //console.log(Type);
 
-
 //let number=89;
 //let Type=number >= 90 ? "gold" : "silver";
 //console.log(Type);
-
 
 //----------------if else prac----------
 //let hour = 19 ;
@@ -595,41 +542,38 @@
 //let age =20;
 //if(age >=18){
 //console.log('Elligble')
-//} 
-//else{
-//console.log('Not Elligble')  
 //}
-
+//else{
+//console.log('Not Elligble')
+//}
 
 //--------------Grade-----
 //let grade = 35;
 //if(grade >= 90 && grade <100){
- // console.log('A+1');
+// console.log('A+1');
 //}
 //if(grade >= 80 && grade <90){
 //  console.log('A1');
 //}
 //if(grade >= 70 && grade <80){
-  //console.log('A');
+//console.log('A');
 //}
 //if(grade >= 60 && grade <70){
- // console.log('B');
+// console.log('B');
 //}
 //if(grade >= 50 && grade <60){
- // console.log('c');
+// console.log('c');
 //}
 //if(grade >= 40 && grade <50){
- // console.log('d');
+// console.log('d');
 //}
 //if(grade >= 35 && grade <40){
- // console.log('E');
+// console.log('E');
 //}
 
 //else{
 //  console.log('fail');
 //}
-
-
 
 //-------------less then greater----
 //let n =99;
@@ -640,33 +584,24 @@
 //  console.log('less then 100');
 //}
 
-
-
-
-
 //-----------for loop prac-------------
-
-
 
 //for (let i = 1; i <=10; i++) {
 //  console.log('hello world',i);
-  
-//}
 
+//}
 
 //for (let i = 1; i <= 10; i++) {
 //  if(i %2 !==0)
- // console.log(i);
-  
+// console.log(i);
+
 //}
 
 //for (let i = 1; i <=10; i++) {
- // if(i % 2 ==0)
-  //console.log(i);
-  
+// if(i % 2 ==0)
+//console.log(i);
+
 //}
-
-
 
 //----------while Loop---------------
 //let i = 1;
@@ -675,8 +610,7 @@
 //i++;
 //}
 
-
-//---------------------- 
+//----------------------
 
 //let i = 1
 //while (i<=10) {
@@ -687,26 +621,22 @@
 //let a = 1
 //while (a<=5) {
 //  console.log('hey',a);
- // a++;
-//}  
+// a++;
+//}
 //----------------------
 //let c = 1
 //while (c<=5) {
- // if (c %2 !==0) console.log(c);
- // c++; 
+// if (c %2 !==0) console.log(c);
+// c++;
 //}
 //-----------------------
 //let d = 1
 //while (d <=10) {
 // if(d%2 !==0)console.log(d);
- //d++;
-  //}
+//d++;
+//}
 
-
-
-
-
-//--------Do while loop-----------  
+//--------Do while loop-----------
 
 //--------------------------
 //let a = 1;
@@ -719,8 +649,8 @@
 
 //let c = 1;
 //do {
-// if(c %2 !==0)console.log(c); 
- //c++; 
+// if(c %2 !==0)console.log(c);
+//c++;
 //} while (c <= 10);
 
 //-------------------------
@@ -730,7 +660,6 @@
 //if(d % 2 ==0)console.log(d);
 //d++;
 //} while (d <= 10);
-
 
 //----------New work------
 //-----------For-in---------
@@ -743,19 +672,17 @@
 //for(let key in person)
 //console.log(key,person[key]);
 
-
 //----------
 
 //let colors =['Red','Green','orange'];
 //for(let i in colors)
 //console.log(i,colors[i]);
 
-//---------  
+//---------
 
 //let item = ['headphone','flage','color'];
 //for(let a in item)
 //console.log(a,item[a]);
-
 
 //----------------------
 //let person ={
@@ -772,7 +699,6 @@
 //for ( let a of item ) {
 //console.log(a);
 //}
- 
 
 //------------Break & continue-------------
 //----------while loop-----------
@@ -788,15 +714,9 @@
 //a++;
 //}
 
-
-
-
-
-
 //-------------------------13-10-23
 
 //--------Function------
-
 
 //function oct (Name,lastname){
 //console.log('hello'+" "+Name+" "+'V.S'+" "+lastname)
@@ -813,7 +733,6 @@
 //}
 //wc('Pakistan','India')
 
-
 //function wc (team1,team2,team3,team4){
 //console.log(team1+" "+'Will Face'+" "+team2+" "+'then'+" "+team3+" "+'and'+" "+team4)
 //}
@@ -824,11 +743,10 @@
 //}
 //summary('won','Lose')
 
-
 //-------Tenary operater----
 
 //let num1 =4.1;
-//let Type= num1 > 4.00  ? "First Team" : "Second Team"; 
+//let Type= num1 > 4.00  ? "First Team" : "Second Team";
 //console.log(Type);
 
 //let point = 80;
@@ -836,7 +754,6 @@
 //console.log(Type);
 
 //------------if Else--------------
-
 
 //let hours =23;
 //if (hours >=6 && hours <12) {
@@ -849,12 +766,10 @@
 //console.log('Good Night');
 //}
 
-
 //function result(person,){
 //console.log('sorry'+" "+person+" "+'Your result is');
 //}
 //result('Alex');
-
 
 //let grade =33;
 //if (grade >=90 && grade <100) {
@@ -877,7 +792,6 @@
 //}
 //else {console.log('Fail');}
 
-
 //--------------------less then greater then--------------
 //let n =99;
 //if (n >=100 && n >35){
@@ -887,53 +801,47 @@
 //console.log('less then 100');
 //}
 
-
 //let a = 7;
 //let b=6;
 //if (a > b) {
-//console.log('a greater then b');  
+//console.log('a greater then b');
 //}
 //else if(a === b){
 //console.log('a is equal to b')
 //}
 //else{
-//console.log('b is greater');  
+//console.log('b is greater');
 //}
-
-
 
 //-----------for loop prac-------------
 
-
-
 //for (let i = 1; i <=5; i++) {
 //console.log('hello world',i);
-  
-//}
 
+//}
 
 //for (let i = 1; i <= 5; i++) {
 //if(i %2 !==0)
-  
+
 //console.log(i);
-  
+
 //}
 
 //for (let i = 1; i <=5; i++) {
 // if(i % 2 ==0)
 //console.log(i);
-  
+
 //}
 
 //for (let i = 1; i <= 5; i++) {
 //console.log('hello paajeet',i);
-  
+
 //}
 
 //for (let i = 1; i < 10; i++) {
 // if(i %2 ==0)
 // console.log(i);
-  
+
 //}
 
 //for (let i = 1; i < 10; i++){
@@ -961,7 +869,7 @@
 //n++;
 //}
 
-//--------Do while loop-----------  
+//--------Do while loop-----------
 
 //--------------------------
 //let a = 1;
@@ -973,15 +881,15 @@
 //-----------
 //let b =1;
 //do {
-//console.log('heyyooo',b) 
-//b++;  
+//console.log('heyyooo',b)
+//b++;
 //} while (b <= 5);
 
 //--------------------
 //let c = 1;
 //do {
 //if(c %2 !==0)console.log(c);
-//c++;  
+//c++;
 //} while (c <=10);
 
 //-----------For-in---------
@@ -997,17 +905,16 @@
 //let p ={
 //name:'Lod',
 //age:20,
-//location:'moon',  
+//location:'moon',
 //};
-//for (let key in p) 
-   
+//for (let key in p)
+
 //console.log(key,p[key]);
-    
-  
+
 // let person1 = {
 // name:'jiona',
 // age:25,
-// designation:'1st Dev full stack',  
+// designation:'1st Dev full stack',
 // };
 
 // for (const a in person1) {
@@ -1032,14 +939,10 @@
 // console.log(a,person3[a]);
 // }
 
-
-
 // let item = ['mobile','headphones','charger'];
 // for ( let a of item ) {
 // console.log(a);
 // }
-
-
 
 //----------
 
@@ -1047,12 +950,11 @@
 //for(let i in colors)
 //console.log(i,colors[i]);
 
-//---------  
+//---------
 
 //let item = ['headphone','flage','color'];
 //for(let a in item)
 //console.log(a,item[a]);
-
 
 //----------------------
 
@@ -1077,7 +979,6 @@
 //console.log(d);
 //}
 
-
 //------------Break & continue-------------
 
 //for (let i= 1; i < 10; i++) {
@@ -1094,13 +995,11 @@
 
 // let a = 1;
 // while (a <= 10) {
-// if(a === 6)break;          
+// if(a === 6)break;
 
 // console.log(a);
 // a++;
 // }
-
-
 
 //---------------------14-10-23
 //--------Function------
@@ -1118,12 +1017,10 @@
 //-------Tenary operater----
 
 //let runrate =4.1;
-//let Type= runrate > 4.00  ? "First Team" : "Second Team"; 
+//let Type= runrate > 4.00  ? "First Team" : "Second Team";
 //console.log(Type);
 
-
 //------------if Else--------------
-
 
 //let hours =23;
 //if (hours >=6 && hours <12) {
@@ -1135,7 +1032,6 @@
 //else{
 //console.log('Good Night');
 //}
-
 
 // let grade =90;
 // if (grade >=90 && grade <100) {
@@ -1158,45 +1054,58 @@
 // }
 // else {console.log('Fail');}
 
-
 //--------------------less then greater then--------------
-let n =99;
-if (n >=100 && n >35){
-onsole.log('100 is greater');
+// let n =99;
+// if (n >=100 && n >35){
+// onsole.log('100 is greater');
+// }
+// else{
+// console.log('less then 100');
+// }
+
+// let a = 7;
+// let b=6;
+// if (a > b) {
+// console.log('a greater then b');
+// }
+// else if(a === b){
+// console.log('a is equal to b')
+// }
+// else{
+// console.log('b is greater');
+// }
+
+//----------------15-10-23
+
+//-----------for loop prac-------------
+
+//for (let i = 1; i <=5; i++) {
+//console.log('hello world',i);
+
+//}
+
+//for (let i = 1; i <= 5; i++) {
+//if(i %2 !==0)
+
+//console.log(i);
+
+//}
+
+//for (let i = 1; i <=5; i++) {
+// if(i % 2 ==0)
+//console.log(i);
+
+//}
+
+//for (let i = 1; i <= 5; i++) {
+//console.log('hello paajeet',i);
+
+//}
+
+for (let i = 1; i < 10; i++) {
+  if (i % 2 == 0) console.log(i);
 }
-else{
-console.log('less then 100');
+
+for (let i = 1; i < 10; i++) {
+  if (i % 2 !== 0) console.log(i);
 }
-
-
-let a = 7;
-let b=6;
-if (a > b) {
-console.log('a greater then b');  
-}
-else if(a === b){
-console.log('a is equal to b')
-}
-else{
-console.log('b is greater');  
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
