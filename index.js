277ec479051b70c591982b05345b8afddf3c2124
Uzair0331 <uxair0331xhaikh@gmail.com>
2023-10-26 22:54:00 +0500

    obj

--- index.js
+++ index.js
@@ -4133,11 +4133,39 @@
 // }
 
 //------------------------------------
- function address(detail) {
-     this.street = 15 ;
-     this.City='italy';
-     this.zipCode=9854;   
-    }
-    const address1 = new address(1);
-    console.log(address1)
 
+
+// function address(detail) {
+// this.street = 15 ;
+// this.City='italy';
+// this.zipCode=9854;   
+// }
+// const address1 = new address(1);
+// console.log(address1)
+
+//-------------------------------------
+
+
+let address1 = new address('a','b','c');
+let address2 = new address('a','b','c');
+
+console.log(areEqual(address1,address2));
+console.log(areSame(address1,address2));
+
+function address(street,city,zipCode) {
+   this.street= street;
+   this.city=city;
+   this.zipCode=zipCode; 
+}
+
+function areEqual(address1,address2) {
+ return(
+ address1.street === address2.street &&
+ address1.city === address2.city &&
+ address1.zipCode === address2.zipCode 
+ )
+}
+
+function areSame(address1,address2) {
+return(address1 === address2)    
+}
