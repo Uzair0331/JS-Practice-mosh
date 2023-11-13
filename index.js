28cbc088bf0cc760ebf19e2920509f461c45c65b
uzair0331 <uxair0331xhaikh@gmail.com>
2023-11-13 21:33:20 +0500

    dsa

--- index.js
+++ index.js
@@ -6319,19 +6319,20 @@
 //     }
 
 //-----------------------------
-function compareTriplets(a, b) {
-    let alice = 0;
-    let bob = 0;
-    for (let i = 0; i < 3; i++) {
-        if (a[i] > b[i]) {
-            alice++;
-        }
-        if (b[i] > a[i]) {
-            bob++;
-        }
-    }
-    return [alice, bob]
-}
+// function compareTriplets(a, b) {
+//     let alice = 0;
+//     let bob = 0;
+//     for (let i = 0; i < 3; i++) {
+//         if (a[i] > b[i]) {
+//             alice++;
+//         }
+//         if (b[i] > a[i]) {
+//             bob++;
+//         }
+//     }
+//     return [alice, bob]
+// }
+
 
 
 
