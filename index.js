5b560be133806e629dafe3564c8e632d597ee423
uzair0331 <uxair0331xhaikh@gmail.com>
2023-11-13 19:11:32 +0500

    pil

--- index.js
+++ index.js
@@ -6319,10 +6319,19 @@
 //     }
 
 //-----------------------------
-function circularArrayRotation(a, k, queries) {
-
+function compareTriplets(a, b) {
+    let alice = 0;
+    let bob = 0;
+    for (let i = 0; i < 3; i++) {
+        if (a[i] > b[i]) {
+            alice++;
+        }
+        if (b[i] > a[i]) {
+            bob++;
+        }
+    }
+    return [alice, bob]
 }
-
 
 
 
