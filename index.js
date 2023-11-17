72ea57409c50ce5ba4ca2843ce94ddc7664216d1
uzair0331 <uxair0331xhaikh@gmail.com>
2023-11-17 16:28:59 +0500

    ew

--- index.js
+++ index.js
@@ -6498,7 +6498,7 @@ function timeConversion(s) {
 function divisibleSumPairs(n, k, ar) {
    let count = 0;
    for (let i = 0; i < n - 1; i++) {
-
+      for (let j = i + 1; j < n; j++) { }
    }
 
 }
