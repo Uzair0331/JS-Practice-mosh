7f7119d342ea9ddb2f588464ac1e80ff0d470626
uzair0331 <uxair0331xhaikh@gmail.com>
2023-11-18 02:42:28 +0500

    ewq

--- index.js
+++ index.js
@@ -6538,13 +6538,12 @@ n = 6,
    function divisibleSumPairs(n, k, ar) {
 
 
+      let count = 0;
 
-      for (let i = 0; i < n - 1; i++) {
-         for (let j = i + 1; j < n; j++) {
-            if ((ar[i] + ar[j]) % k == 0) { count++ }
-         }
+      if ((ar[i] + ar[j]) % k == 0) { count++ }
+   }
       }
-      return count;
+return count;
    }
 
 
