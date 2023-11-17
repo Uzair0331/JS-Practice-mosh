ef4b4321e0c11ca742a4a03a5bafb76a8b9b202d
uzair0331 <uxair0331xhaikh@gmail.com>
2023-11-18 02:42:45 +0500

    dsa

--- index.js
+++ index.js
@@ -6539,11 +6539,12 @@ n = 6,
 
 
       let count = 0;
-
-      if ((ar[i] + ar[j]) % k == 0) { count++ }
-   }
+      for (let i = 0; i < n - 1; i++) {
+         for (let j = i + 1; j < n; j++) {
+            if ((ar[i] + ar[j]) % k == 0) { count++ }
+         }
       }
-return count;
+      return count;
    }
 
 
