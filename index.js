200c2e029c57e93c391c17afa898884545c64951
uzair0331 <uxair0331xhaikh@gmail.com>
2023-11-18 02:41:32 +0500

    dsa

--- index.js
+++ index.js
@@ -6499,18 +6499,21 @@
 
 
 
+n = 6,
+   k = 3,
+   ar = [1, 3, 2, 6, 1, 2],
+   function divisibleSumPairs(n, k, ar) {
 
-function divisibleSumPairs(n, k, ar) {
-   n = 6;
-   k = 3;
-   ar = [1, 3, 2, 6, 1, 2];
 
-   let count = 0;
-   for (let i = 0; i < n - 1; i++) {
-      for (let j = i + 1; j < n; j++) {
-         if ((ar[i - ar[j]]) % k == 0) { count++ }
+      let count = 0;
+      for (let i = 0; i < n - 1; i++) {
+         for (let j = i + 1; j < n; j++) {
+            if ((ar[i] + ar[j]) % k == 0) { count++ }
+         }
       }
+      return count;
    }
-   return count;
-}
-divisibleSumPairs()
+
+
+
+
