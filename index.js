40512391a60a5d6ac2707637badcc1f0312a6bb8
Uzair0331 <uxair0331xhaikh@gmail.com>
2023-11-19 21:09:24 +0500

    ds

--- index.js
+++ index.js
@@ -6251,3 +6251,54 @@ function diagonalDifference(arr) {
   }
   return Math.abs(d1 - d2);
 }
+
+//----------------------------------
+//-----------plus-mius--------------
+function plusMinus(arr) {
+    let plusVal = 0;
+let negVal = 0;
+let zero = 0;
+let arrLength = arr.length;
+
+for (let i = 0; i < arrLength; i++) {
+  if (arr[i] > 0) {
+    plusVal++;
+  } else if (arr[i] < 0) {
+    negVal++;
+  } else {
+    zero++;
+  }
+}
+let posAns = (plusVal / arrLength).toFixed(6);
+let negAns = (negVal / arrLength).toFixed(6);
+let zeroAns = (zero / arrLength).toFixed(6);
+
+console.log(posAns);
+console.log(negAns);
+console.log(zeroAns);
+}
+
+
+function plusMinus(arr) {
+  let plusVal = 0;
+let negVal = 0;
+let zero = 0;
+let arrLength = arr.length;
+
+for (let i = 0; i < arrLength; i++) {
+if (arr[i] > 0) {
+  plusVal++;
+} else if (arr[i] < 0) {
+  negVal++;
+} else {
+  zero++;
+}
+}
+let posAns = (plusVal / arrLength).toFixed(6);
+let negAns = (negVal / arrLength).toFixed(6);
+let zeroAns = (zero / arrLength).toFixed(6);
+
+console.log(posAns);
+console.log(negAns);
+console.log(zeroAns);
+}
