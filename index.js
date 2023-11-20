1608bfcbdc1446defa2faa400b8cdef212817f32
uzair0331 <uxair0331xhaikh@gmail.com>
2023-11-20 18:12:51 +0500

    das

--- index.js
+++ index.js
@@ -6546,29 +6546,29 @@ function divisibleSumPairs(n, k, ar) {
 
 //----------------------------------
 //-----------plus-mius--------------
-function plusMinus(arr) {
-   let plusVal = 0;
-   let negVal = 0;
-   let zero = 0;
-   let arrLength = arr.length;
+// function plusMinus(arr) {
+//    let plusVal = 0;
+//    let negVal = 0;
+//    let zero = 0;
+//    let arrLength = arr.length;
 
-   for (let i = 0; i < arrLength; i++) {
-      if (arr[i] > 0) {
-         plusVal++;
-      } else if (arr[i] < 0) {
-         negVal++;
-      } else {
-         zero++;
-      }
-   }
-   let posAns = (plusVal / arrLength).toFixed(6);
-   let negAns = (negVal / arrLength).toFixed(6);
-   let zeroAns = (zero / arrLength).toFixed(6);
+//    for (let i = 0; i < arrLength; i++) {
+//       if (arr[i] > 0) {
+//          plusVal++;
+//       } else if (arr[i] < 0) {
+//          negVal++;
+//       } else {
+//          zero++;
+//       }
+//    }
+//    let posAns = (plusVal / arrLength).toFixed(6);
+//    let negAns = (negVal / arrLength).toFixed(6);
+//    let zeroAns = (zero / arrLength).toFixed(6);
 
-   console.log(posAns);
-   console.log(negAns);
-   console.log(zeroAns);
-}
+//    console.log(posAns);
+//    console.log(negAns);
+//    console.log(zeroAns);
+// }
 
 
 
