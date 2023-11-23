c267f26387107f33ea7183300abe1c36cab2ef01
uzair0331 <uxair0331xhaikh@gmail.com>
2023-11-23 18:08:14 +0500

    ewq

--- index.js
+++ index.js
@@ -6600,6 +6600,17 @@
 //     }
 //     return [alice, bob]
 //---------------
+//-----------------Number line jump----------
+// function kangaroo(x1, v1, x2, v2) {
+//     if (v1 < v2)
+//     return "NO";
+//     if((x2 - x1) % (v1 - v2) == 0)
+//     return "YES";
+//     else
+//     return "NO";
+
+//  }
+
 
 
 //----------------
