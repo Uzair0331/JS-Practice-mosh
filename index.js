35475af4fe0e0fd33c21e13f1517065557994281
uzair0331 <uxair0331xhaikh@gmail.com>
2023-11-24 16:57:30 +0500

    kjh

--- index.js
+++ index.js
@@ -6774,5 +6774,9 @@ function miniMaxSum(arr) {
     let min = arr[0]
     let max = arr[0]
     let sum = 0;
-
+    for (let i = 0; i < arr.length; i++) {
+        if (min > arr[i]) {
+            min === arr[i]
+        }
+    }
 }
