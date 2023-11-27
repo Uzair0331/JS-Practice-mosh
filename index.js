80bf80699402f4d442692135085a6b45acff83dc
uzair0331 <uxair0331xhaikh@gmail.com>
2023-11-27 18:47:03 +0500

    yt

--- index.js
+++ index.js
@@ -6807,21 +6807,32 @@
 //     }
 //     console.log(sum)
 // }
-// miniMaxSum(arr)
+
+//----------------------------------------------
 
 
-// let arr = [1, 3, 5, 7, 9]
-// function miniMaxSum(arr) {
-//     let min = arr[0]
-//     let max = arr[0]
-//     let sum = 0;
-//     for (let i = 0; i < arr.length; i++) {
-//         if (min > arr[i]) {
-//             (min === arr[i])
-//         }
-//         sum++;
 
-//     }
-//     console.log(sum)
-// }
-// miniMaxSum(arr)
+
+
+let arr = [9, 1, 3, 5, 7,]
+function miniMaxSum(arr) {
+    arr.sort();
+    let min = arr[0]
+    let max = arr[0]
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (min > arr[i]) {
+            (min === arr[i])
+        }
+        if (max < arr[i]) {
+            (max === arr[i])
+        }
+        sum += arr[i]
+
+    }
+    console.log(sum)
+}
+miniMaxSum(arr)
+
+
+
