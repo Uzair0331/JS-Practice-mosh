0598946f8d46d82c67c3be9df5de54ec9b682bbc
uzair0331 <uxair0331xhaikh@gmail.com>
2023-11-28 19:14:11 +0500

    rwq

--- index.js
+++ index.js
@@ -6877,7 +6877,9 @@ function kangaroo(x1, v1, x2, v2) {
     // else {
     //     return "NO"
     // }
-
+    if (x1 < x2) {
+        return "NO"
+    }
     if ((x1 - x2) % (v1 - v2) == 0) {
         return "YES"
     }
