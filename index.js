12f3ca68ac7ffe845dfba6008e7a29509a4c8e8b
uzair0331 <uxair0331xhaikh@gmail.com>
2023-11-28 19:57:15 +0500

    das

--- index.js
+++ index.js
@@ -6582,7 +6582,23 @@
 //    console.log(totalOranges)
 // }
 
-
+// //--------
+// var n = matrix.length;
+// var diag1 = 0;
+// var diag2 = 0;
+// for(var i=0; i<n; i++){
+//     for(var j=0; j<n; j++){
+//         // an element from the main diagonal
+//         if(i === j) { 
+//             diag1 += matrix[i][j];
+//         }
+//         // an element from the counterdiagonal
+//         if(i + j === n - 1){
+//             diag2 += matrix[i][j];
+//         }
+//     }
+// }
+// return Math.abs(diag1 - diag2);
 
 
 
@@ -6864,27 +6880,27 @@
 
 
 
-let x1 = 0;
-let v1 = 2;
-let x2 = 5;
-let v2 = 3;
-function kangaroo(x1, v1, x2, v2) {
-    // let valueOne = x1 - x2;
-    // let valueTwo = v1 - v2;
-    // if ((valueOne % valueTwo) == 0) {
-    //     return "YES"
-    // }
-    // else {
-    //     return "NO"
-    // }
-    if (x1 < x2) {
-        return "NO"
-    }
-    if ((x1 - x2) % (v1 - v2) == 0) {
-        return "YES"
-    }
-    else {
-        return "NO"
-    }
-}
-kangaroo(x1, v1, x2, v2)
+// let x1 = 0;
+// let v1 = 2;
+// let x2 = 5;
+// let v2 = 3;
+// function kangaroo(x1, v1, x2, v2) {
+// let valueOne = x1 - x2;
+// let valueTwo = v1 - v2;
+// if ((valueOne % valueTwo) == 0) {
+//     return "YES"
+// }
+// else {
+//     return "NO"
+// }
+//     if (v1 < v2) {
+//         return "NO"
+//     }
+//     if ((x1 - x2) % (v1 - v2) == 0) {
+//         return "YES"
+//     }
+//     else {
+//         return "NO"
+//     }
+// }
+// kangaroo(x1, v1, x2, v2)
