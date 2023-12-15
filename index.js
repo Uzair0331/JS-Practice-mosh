965f308216a85caebd08e5c326d6faecd781961b
uzair0331 <uxair0331xhaikh@gmail.com>
2023-12-16 00:05:41 +0500

    asd

--- index.js
+++ index.js
@@ -7456,17 +7456,17 @@
 
 
 // --------------------------------------
-let arrLength = 5;
+
 let arr = [1, 4, 5, 3, 2];
 let m = 2;
 function icecreamParlor(m, arr) {
 
-    for (let i = 0; i < arrLength; i++) {
-        for (let j = 0; j < arrLength; j++) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
             if (
                 arr[i] + arr[j] === m
             ) {
-                return (i + j)
+                return (i + 1, j + 1)
             }
         }
     }
