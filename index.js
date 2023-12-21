205d3ae446f4142193452f6c9bfc6f42c5a74228
uzair0331 <uxair0331xhaikh@gmail.com>
2023-12-21 16:38:15 +0500

    adf

--- index.js
+++ index.js
@@ -7515,24 +7515,44 @@
 
 //----------------------------
 
-a = [6, 4, 1]
-function countSwaps(a) {
-    let swapingCount = 0;
-    for (let i = 0; i < a.length; i++) {
-        for (let j = 0; j < a.length - i - 1; j++) {
-            console.log("Array a [i]" + a[i])
-            console.log("Array a [j]" + a[j])
-            if (a[j] > a[j - 1]) {
-                swapingCount++;
-            }
-        }
-        console.log(a)
-    }
-    console.log(a)
-    console.log("Array is sorted in = " + swapingCount + " swaps")
-    console.log("First Element ===> " + a[0])
-    console.log("last Element ===>" + a[a.length - 1])
+// a = [6, 4, 1]
+// function countSwaps(a) {
+//     let swapingCount = 0;
+//     for (let i = 0; i < a.length; i++) {
+//         for (let j = 0; j < a.length - i; j++) {
+//             console.log("Array a [i]" + a[i])
+//             console.log("Array a [j]" + a[j])
+//             if (a[j] > a[j - 1]) {
+//                 swapingCount++;
+//             }
+//         }
+//         console.log(a)
+//     }
+//     console.log(a)
+//     console.log("Array is sorted in = " + swapingCount + " swaps")
+//     console.log("First Element ===> " + a[0])
+//     console.log("last Element ===>" + a[a.length - 1])
 
-}
-countSwaps(a)
+// }
+// countSwaps(a)
 
+// a = [6, 4, 1]
+// function countSwaps(a) {
+//     let swapingCount = 0;
+//     for (let i = 0; i < a.length; i++) {
+//         for (let j = 0; j < a.length - i; j++) {
+//             console.log("Array a [i]" + a[i])
+//             console.log("Array a [j]" + a[j])
+//             if (a[j] > a[j - 1]) {
+//                 swapingCount++;
+//             }
+//         }
+//         console.log(a)
+//     }
+//     console.log(a)
+//     console.log("Array is sorted in = " + swapingCount + " swaps")
+//     console.log("First Element ===> " + a[0])
+//     console.log("last Element ===>" + a[a.length - 1])
+
+// }
+// countSwaps(a)
